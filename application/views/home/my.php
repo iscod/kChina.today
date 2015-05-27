00476bc15cacbcbfc50dcf11fa0f9a1128dffdab
宁腾磊 <iscod@ningtengleideMacBook-Pro.local>
2015-05-27 20:03:23 +0800

    page jquery.funytext.js

---
+++ application/views/home/my.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Welcome kChina</title>
+	<meta name="description" content="免费图书、分享旅行、爱情、话题的文化长廊">
+	<script src="http://js.<?php echo HOST_DOMAIN?>/js/ess.min.js<?php echo JS_VERSION?>"></script>
+	<link rel="stylesheet" id="dashicons-css" href="http://css.<?php echo HOST_DOMAIN?>/css/style.css?ver=<?php echo CSS_VERSION?>" type="text/css" media="all">
+	<link rel="stylesheet" id="dashicons-css" href="http://css.<?php echo HOST_DOMAIN?>/css/book.css?ver=<?php echo CSS_VERSION?>" type="text/css" media="all">
+</head>
+</head>
+<body>
+	<?php $this->load->view('header')?>
+</body>
